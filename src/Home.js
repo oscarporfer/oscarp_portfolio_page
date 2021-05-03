18d867e7608a4009cf618d1f2bbd606329b5b825
oscarporfer <oscarfportes@gmail.com>
2021-05-03 13:59:05 -0400

    Updated

--- src/Home.js
+++ src/Home.js
@@ -16,21 +16,57 @@ const Home = () => {
         <button className="home__contact__btn">Contact Me</button>
       </div>
       <div className="home__skills">
-        <p>Tools & Frameworks</p>
+        <p> Tools and Frameworks</p>
       </div>
-      <div className="name">
-        <h4>React
-          <p>Frame Work for</p>
-        </h4>
-        <h4>React
-          <p>Frame Work for</p>
-        </h4>
-        <h4>React
-          <p>Frame Work for</p>
-        </h4>
-        <h4>React
-          <p>Frame Work for</p>
-        </h4>
+      <div className="home__skills__list">
+       <article>
+         <h2>JavaScript/JS</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>React</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>HTML/HTML5</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>Express.js</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>jQuery</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>API Development</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>Git</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>Heroku</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>VS Code</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>jGoogle Firebase</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>Material UI</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
+       <article>
+         <h2>Git</h2>
+         <p>Description Description Description Description Description Description Description Description</p>
+       </article>
       </div>
     </div>
   );
